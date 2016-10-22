baa433c949cf61d4632dfdc03ed08f0f14fdbfb7
Randall Porter <porterrandall@yahoo.com>
2016-10-22 11:28:42 -0700

    Lab4 complete

---
+++ cst356lab4/scripts/ABCscript.js
@@ -0,0 +1,15 @@
+﻿function submit() {
+
+    var elements = document.getElementById("mainForm").elements;
+    var obj = {};
+    for (var i = 0 ; i < elements.length ; i++) {
+        var item = elements.item(i);
+        if (item.value == '') {
+            alert(item.name + ' is required!');
+            return false;
+        }
+        obj[item.name] = item.value;
+    }
+
+    document.getElementById("divResults").innerHTML = JSON.stringify(obj);
+}
